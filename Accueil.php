db653579163fb5db5f4c9b3bfe2365ba45dcd329
AmineFilaliAnsari <aminefilaliansari@gmail.com>
2020-04-12 19:26:36 +0100

    06

--- Accueil.php
+++ Accueil.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Accueil</title>
+	<title>Accueil06</title>
      <!--===============================================================================================-->
   <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
