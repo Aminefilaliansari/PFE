592b3010ade6c3211163320b967a12569b9eca1f
AmineFilaliAnsari <aminefilaliansari@gmail.com>
2020-04-14 20:41:12 +0100

    New

--- MenuDesign.php
+++ MenuDesign.php
@@ -7,7 +7,7 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="main.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">   
 
